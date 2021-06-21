d75ea07c2e5fa4b4b7bc87ddb10c6f08a6ed072e
ongkypramuja <pramujaongky@gmail.com>
2021-06-22 00:00:13 +0700

    baru

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,8 +20,8 @@ class UsersTableSeeder extends Seeder
             0 => 
             array (
                 'id' => 1,
-                'full_name' => 'John Doe',
-                'email' => 'john@examle.com',
+                'full_name' => 'suarti',
+                'email' => 'suarti@gmail.com',
                 'password' => '12345',
                 'prev_password' => NULL,
                 'address_id' => 1,
@@ -29,42 +29,7 @@ class UsersTableSeeder extends Seeder
                 'created_at' => '2019-02-09',
                 'updated_at' => '2019-02-09',
             ),
-            1 => 
-            array (
-                'id' => 2,
-                'full_name' => 'Shakib Mostahid',
-                'email' => 'b@gmail.com',
-                'password' => '12345',
-                'prev_password' => NULL,
-                'address_id' => 2,
-                'phone' => '01922991100',
-                'created_at' => '2019-02-11',
-                'updated_at' => '2019-02-11',
-            ),
-            2 => 
-            array (
-                'id' => 3,
-                'full_name' => 'Nishat Ashraf',
-                'email' => 'c@gmail.com',
-                'password' => '12345',
-                'prev_password' => NULL,
-                'address_id' => 3,
-                'phone' => '01899001144',
-                'created_at' => '2019-02-11',
-                'updated_at' => '2019-02-11',
-            ),
-            3 => 
-            array (
-                'id' => 4,
-                'full_name' => 'Ayomi Hridy',
-                'email' => 'd@gmail.com',
-                'password' => '12345',
-                'prev_password' => NULL,
-                'address_id' => 4,
-                'phone' => '0111111111',
-                'created_at' => '2019-02-11',
-                'updated_at' => '2019-02-11',
-            ),
+            
         ));
         
         
