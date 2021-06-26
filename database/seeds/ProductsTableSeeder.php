625f32149d6df06099808228d207547c3b1cff95
ongkypramuja <pramujaongky@gmail.com>
2021-06-26 19:06:31 +0700

    baruuu

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -17,7 +17,22 @@ class ProductsTableSeeder extends Seeder
         \DB::table('products')->delete();
         
         \DB::table('products')->insert(array (
-           
+            0 => 
+            array (
+                'id' => 1,
+            'name' => 'klepon',
+                'image_name' => 'basah01.jpg',
+                'description' => '<div class="ng-scope">
+<p>rasanyan enak</p>
+</div>',
+                'colors' => '#c0c0c0',
+                'price' => 214200,
+                'discount' => 205000,
+                'tag' => 'New',
+                'category_id' => 1,
+                'created_at' => '2019-02-09',
+                'updated_at' => '2019-02-09',
+            ),
             
         ));
         
